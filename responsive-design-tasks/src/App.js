7fb87c156a4a6edd818ea303365d04fadc9b15b8
zuveriya-spreadd <zuveriya.munshi@spreadd.io>
2025-01-20 18:13:08 +0530

    [TASKS] Responsive Design tasks with React Added

---
+++ responsive-design-tasks/src/App.js
@@ -0,0 +1,76 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='logo'>My Website </div>
+          <nav>
+          <ul className='nav-items'>
+          <li><a href='#'>Link 1</a></li>
+          <li><a href='#'>Link 2</a></li>
+          <li><a href='#'>Link 3</a></li>
+          </ul>
+        </nav>             
+      </header>
+      <main>
+      <article className=''>
+        <h1>This is the main Heading</h1>
+        <p>
+          Ex do laboris est occaecat exercitation aliquip eiusmod in laboris nulla pariatur commodo.
+        </p>
+        <p>Commodo excepteur non in adipisicing ipsum enim aliquip adipisicing exercitation eiusmod laboris laboris anim. Ex est laboris et sit dolor dolor. Esse consequat nulla ut cupidatat deserunt voluptate excepteur aute duis elit dolor aliquip eu enim. Fugiat laboris veniam nostrud mollit eu consectetur laboris magna officia ad mollit aute fugiat. Commodo excepteur laborum adipisicing id magna officia.</p>
+      </article>
+      
+  <aside className='sider'>
+    <p>
+    Have you discovered all of the other excellent content on this
+    website?
+    </p>
+  </aside>
+  <ul className='cards'>
+        <li>
+          <h2>Card One</h2>
+        <div className='card'>
+        <p>
+            Irure aliquip occaecat culpa eu culpa qui labore voluptate elit. Non veniam sit nisi magna tempor velit aliquip anim nulla non dolor ea deserunt aliquip. Labore pariatur et dolor ut. Voluptate aliquip fugiat id quis. Veniam amet culpa tempor anim culpa minim magna ea fugiat elit. Non amet id amet enim esse mollit est aute. Anim dolore laboris aliqua eiusmod elit velit ut.
+          </p>
+           </div>
+        </li>
+        <li>
+          <h2>Card Two</h2>
+        <div className='card'>
+        <p>
+            Irure aliquip occaecat culpa eu culpa qui labore voluptate elit. Non veniam sit nisi magna tempor velit aliquip anim nulla non dolor ea deserunt aliquip. Labore pariatur et dolor ut. Voluptate aliquip fugiat id quis. Veniam amet culpa tempor anim culpa minim magna ea fugiat elit. Non amet id amet enim esse mollit est aute. Anim dolore laboris aliqua eiusmod elit velit ut.
+          </p>
+           </div>
+        </li>
+        <li>
+          <h2>Card Three</h2>
+        <div className='card'>
+        <p>
+            Irure aliquip occaecat culpa eu culpa qui labore voluptate elit. Non veniam sit nisi magna tempor velit aliquip anim nulla non dolor ea deserunt aliquip. Labore pariatur et dolor ut. Voluptate aliquip fugiat id quis. Veniam amet culpa tempor anim culpa minim magna ea fugiat elit. Non amet id amet enim esse mollit est aute. Anim dolore laboris aliqua eiusmod elit velit ut.
+          </p>
+           </div>
+        </li>
+        <li>
+          <h2>Card Four</h2>
+        <div className='card'>
+        <p>
+            Irure aliquip occaecat culpa eu culpa qui labore voluptate elit. Non veniam sit nisi magna tempor velit aliquip anim nulla non dolor ea deserunt aliquip. Labore pariatur et dolor ut. Voluptate aliquip fugiat id quis. Veniam amet culpa tempor anim culpa minim magna ea fugiat elit. Non amet id amet enim esse mollit est aute. Anim dolore laboris aliqua eiusmod elit velit ut.
+          </p>
+           </div>
+        </li>
+        <li>
+          <h2>Card Five</h2>
+        <div className='card'>
+        <p>
+            Irure aliquip occaecat culpa eu culpa qui labore voluptate elit. Non veniam sit nisi magna tempor velit aliquip anim nulla non dolor ea deserunt aliquip. Labore pariatur et dolor ut. Voluptate aliquip fugiat id quis. Veniam amet culpa tempor anim culpa minim magna ea fugiat elit. Non amet id amet enim esse mollit est aute. Anim dolore laboris aliqua eiusmod elit velit ut.
+          </p>
+           </div>
+        </li>
+      </ul>
+  </main>
+  </div>
+  );
+}
+export default App;
